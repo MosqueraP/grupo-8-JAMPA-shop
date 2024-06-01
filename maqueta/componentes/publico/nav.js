3da186b9c80ad1fcfa5f392f28970bdfb0fd64e8
Andres.Sandoval123 <andres.sandoval@genstudents.org>
2024-06-01 04:20:17 -0500

    modificacion de carpetas  y creacion de head admin

--- maqueta/componentes/publico/nav.js
+++ maqueta/componentes/publico/nav.js
@@ -5,19 +5,18 @@ encabezadohead.innerHTML = `
 <div class='contenido'>
 
 <div class='logo'>
-<a href='/maqueta/index.html'><img src='/maqueta/imagenes/logos/logonaranja.png' alt=''></a>
+<a href='/maqueta/paginas/publico/home.html'><img src='https://i.postimg.cc/xdqv2rgS/logo-Naranja.png' alt=''></a>
 </div>
-
 <nav class='nav' id='ham'>
 <ul class='barraNavegacion'>
-    <li class='itemBarra1'><a href='/maqueta/index.html'>INICIO</a></li>
-    <li class='itemBarra2'><a href='/maqueta/nav.html'>PRODUCTOS</a></li>
-    <li class='itemBarra3'><a href='/maqueta/paginas/nosotros.html'>NOSOTROS</a></li>
-    <li class='itemBarra4'><a href='/maqueta/paginas/contactanos.html'>CONTACTANOS</a></li>
+    <li class='itemBarra1'><a href='/maqueta/paginas/publico/home.html'>INICIO</a></li>
+    <li class='itemBarra2'><a href='#'>PRODUCTOS</a></li>
+    <li class='itemBarra3'><a href='/maqueta/paginas/publico/nosotros.html'>NOSOTROS</a></li>
+    <li class='itemBarra4'><a href='/maqueta/paginas/publico/contactanos.html'>CONTACTANOS</a></li>
     <li class='itemCerrarSesion' id='cerrar'><a href='#iniciar'>CERRAR SESION</a></li>
     <li class='itemIniSesion' id='iniciar'><a href='#cerra'>INICIAR SESION</a></li>
 </ul>
-    <a class='icoMenu' href='#ham'><i class='bi bi-list'></i></a>
+    <a class='icoMenu'  href='#ham'><i class='bi bi-list'></i></a>
     <a class='icoCerrar' href='#'><i class='bi bi-x-lg'></i></a>
 </nav>
 <div class='barraBusqueda'>
@@ -26,5 +25,4 @@ encabezadohead.innerHTML = `
 </div>
 <a class='icoUser' href=''><i class='bi bi-person'></i></a>
 <a class='icoCarrito' href=''><i class='bi bi-cart3'></i></a>
-
 </div>`
