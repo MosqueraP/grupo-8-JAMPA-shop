f8f5a8042ba8ada5e6a06b5554b2416afc8812fa
Alexa Bautista <alexa.bautista@genstudents.org>
2024-06-26 00:36:54 -0500

    logica slide home

--- maqueta/scripts/carritoCompras.js
+++ maqueta/scripts/carritoCompras.js
@@ -1,13 +1,13 @@
-const btnAbrirCarrito = document.getElementById("btnAbrirCarrito")
-const btnCerrarCarrito = document.getElementById("btnCerrarCarrito")
-const mainCarrito = document.getElementById("mainCarrito")
+// const btnAbrirCarrito = document.getElementById("btnAbrirCarrito")
+// const btnCerrarCarrito = document.getElementById("btnCerrarCarrito")
+// const mainCarrito = document.getElementById("mainCarrito")
 
-btnAbrirCarrito.addEventListener("click", () => {
-  mainCarrito.style.right ="0";
-})
+// btnAbrirCarrito.addEventListener("click", () => {
+//   mainCarrito.style.right ="0";
+// })
 
-btnCerrarCarrito.addEventListener("click", () =>{
-mainCarrito.style.right="-100%";
-})
+// btnCerrarCarrito.addEventListener("click", () =>{
+// mainCarrito.style.right="-100%";
+// })
 
 
