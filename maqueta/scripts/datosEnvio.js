2e00c92b5fbdf9d930ecee234470a67b55302896
MVICTORIASILVA <m.victoriasilva04@gmail.com>
2024-07-15 19:55:27 -0500

    corregido formulario

--- maqueta/scripts/datosEnvio.js
+++ maqueta/scripts/datosEnvio.js
@@ -1,3 +1,4 @@
+
 // FUNCIÓN PARA MOSTRAR MENSAJE DE CONFIRMACIÓN Y RETENER EL ENVÍO POR DEFECTO DEL FORMULARIO
 document.getElementById('formDatos').addEventListener('submit', function(event) {
     event.preventDefault(); 
@@ -9,4 +10,7 @@ function mostrarMensajeConfirmacion() {
     const mensajeConfirmacion = document.getElementById('mensajeConfirmacion');
     mensajeConfirmacion.style.display = 'block';
 }
+
+
+
 
